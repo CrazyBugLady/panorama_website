d13880e048a2c8dfcf8bf344d7ecb99e6e74f931
Snatsch <=>
2015-04-28 01:17:24 +0200

    better quality of panorama picture

--- app/BusinessObjects/Camera.php
+++ app/BusinessObjects/Camera.php
@@ -1,12 +1,12 @@
 <?php 
 	namespace PanoramaWebsite\BusinessObjects;
 	
-	define ('LEFT', 'http://10.142.126.210/cgi-bin/camctrl.cgi?move=left');
-	define ('RIGHT', 'http://10.142.126.210/cgi-bin/camctrl.cgi?move=right');
-	define ('HOME', 'http://10.142.126.210/cgi-bin/camctrl.cgi?move=home');
-	define ('CUR_PIC', 'http://10.142.126.210/cgi-bin/video.jpg');
-	define ('IMGNR', 4);          //Anzahl gemachter Bilder
-	define ('CUTVAL', 20);        //Pixel welche für einen verbesserten Panoramaeffekt abgeschnitten werden
+	define ('LEFT', 'http://192.168.1.115/cgi-bin/camctrl.cgi?move=left');
+	define ('RIGHT', 'http://192.168.1.115/cgi-bin/camctrl.cgi?move=right');
+	define ('HOME', 'http://192.168.1.115/cgi-bin/camctrl.cgi?move=home');
+	define ('CUR_PIC', 'http://192.168.1.115/cgi-bin/video.jpg');
+	define ('IMGNR', 3);          //Anzahl gemachter Bilder
+	define ('CUTVAL', 0);        //Pixel welche für einen verbesserten Panoramaeffekt abgeschnitten werden
 	
 	/*
 	* Klasse, die dafür zuständig ist, die Interaktion mit der Kamera zu koordinieren
@@ -16,10 +16,10 @@
 		/*
 		* Funktion zur Bewegung der Kamera
 		*/
-		public static function move($times, $direction)
+		public static function move($direction, $times, $speedpan = 5)
 		{
 			for ($time = 1; $time <= $times; $time++) {
-				file_get_contents($direction);
+				file_get_contents($direction . '&speedpan=' . $speedpan);
 			}
 		}
 		
@@ -28,15 +28,27 @@
 		*/
 		public static function savePicture($ArchivePath, $TempPath, $savePictureTemporarily)
 		{
+			$tempPanorama = imagecreatetruecolor(1920, 480);
 			$size = getimagesize($TempPath."/img0.jpg");
-			$baseimg = imagecreatetruecolor($size[0] * IMGNR - (IMGNR*CUTVAL), $size[1]);
+			//$baseimg = imagecreatetruecolor($size[0] * IMGNR - (IMGNR*CUTVAL), $size[1]);
 			for ($i=0; $i < IMGNR; $i++) {
 				$img = imagecreatefromjpeg($TempPath."/img".$i.".jpg"); 
-				imagecopy($baseimg, $img, ($size[0]-CUTVAL)*$i, 0, CUTVAL, 0, $size[0]-CUTVAL
-				, $size[1]);
+				//imagecopy($baseimg, $img, ($size[0]-CUTVAL)*$i, 0, CUTVAL, 0, $size[0]-CUTVAL
+				//, $size[1]);
+				if(CUTVAL > 0) {
+					imagecopy($tempPanorama, $img, ($size[0]-CUTVAL)*$i, 0, 0, 0, $size[0]-CUTVAL, $size[1]);
+				}
+				else
+				{
+					imagecopy($tempPanorama, $img, $size[0]*$i, 0, 0, 0, $size[0], $size[1]);
+				}
+				
 				imagedestroy($img);
 			}
-                
+            
+			$panoramaFull = imagecreatetruecolor(1920, 461);
+			imagecopy($panoramaFull, $tempPanorama, 0, 0, 0, 14, 1920, 461);			
+				
 			$curdate = date("d.m.Y-H-i");
 			$filename = explode("-",$curdate);
                 
@@ -48,11 +60,11 @@
                   mkdir($ArchivePath ."/".$filename[0]."/".$filename[1]);
             }                        
             
-            if($saveTemporarily == false) // soll nur temporär gespeichert werden
+            if($savePictureTemporarily == false) // soll nur temporär gespeichert werden
 			{
-				imagejpeg($baseimg, $ArchivePath ."/".$filename[0]."/".$filename[1]."/".$filename[2].".jpg"); // Folder for Hour and Folder for Minute
+				imagejpeg($panoramaFull, $ArchivePath ."/".$filename[0]."/".$filename[1]."/".$filename[2].".jpg"); // Folder for Hour and Folder for Minute
             }
-			imagejpeg($baseimg, $TempPath ."/temp.jpg");
+			imagejpeg($panoramaFull, $TempPath ."/temp.jpg");
 		}
 				
 		/*
@@ -68,29 +80,29 @@
 		*/
 		public static function startPanorama($ArchivePath, $TempPath, $saveTemporarily)
 		{
-			self::move(LEFT, 0);
-			sleep(5);
+			self::move(HOME, 1, 5);
+			sleep(2);
 			
 			self::takePicture(0, $TempPath);
 			
-			self::move(RIGHT, 0);
-			sleep(2);
+			self::move(RIGHT, 2, -5);
+			self::move(RIGHT, 3, 0);
+			
+			sleep(3);
 			
 			self::takePicture(1, $TempPath);
 			
-			self::move(RIGHT, 0);
-			sleep(2);
+			sleep(3);
+		
+			self::move(RIGHT, 2, -5);
+			self::move(RIGHT, 3, 0);
+			
+			sleep(3);
 			
 			self::takePicture(2, $TempPath);
 			
-			self::move(RIGHT, -5);
-			sleep(2);
-			
-			self::takePicture(3, $TempPath);
-			
 			self::savePicture($ArchivePath, $TempPath, $saveTemporarily);
-			
-			self::move(HOME, -5);
+
 		}
 	}
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	include_once("app/includes/config.php");
 	require_once("app/BusinessObjects/Camera.php");
 	include_once("app/PanoramaManager.php");
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
